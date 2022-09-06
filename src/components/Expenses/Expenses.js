1296959b4fc97d8b0577a267944715823ddc4720
YanaIvanova21 <yana.ivanova.199621@gmail.com>
2022-09-06 12:17:42 +0300

    Rendering list of expenses dynamically.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -18,29 +18,19 @@ function Expenses(props) {
           } /*to control default value of the filtering dropdown*/
           onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          /> //anonymous arrow function with "expense" parameter
+        ))}
       </Card>
     </div>
   );
 }
 
 export default Expenses;
+
+//We can use {} inside the JSX tag to encorporate some JS code.
